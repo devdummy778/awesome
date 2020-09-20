b0568e0fb4f801aafea826362d8d6e6d9ed2ae5f
Fernando <dev.teach.user.dummy@gmail.com>
2020-09-20 01:13:22 -0300

    Adding caroline to user.reference #7

--- users.js
+++ users.js
@@ -2,7 +2,8 @@ const User = [
   {id:1, name:"Steve"},
   {id:2, name:"Ron"},
   {id:3, name:"Francis"},
-  {id:4, name:"Lisa"}
+  {id:4, name:"Lisa"},
+  {id:5, name:"Caroline"},
 ];
 
 const bikes = [
